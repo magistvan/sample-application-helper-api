e241dedecc07bb31fe8e044fa2db0895e6150d62
magistvan <magistvan@yahoo.com>
2024-03-21 20:46:22 +0200

    Fix port number

--- src/main/java/io/github/magistvan/sampleapplicationhelper/api/invoker/SampleApplicationHelperInvoker.java
+++ src/main/java/io/github/magistvan/sampleapplicationhelper/api/invoker/SampleApplicationHelperInvoker.java
@@ -3,6 +3,6 @@ package io.github.magistvan.sampleapplicationhelper.api.invoker;
 import io.github.magistvan.sampleapplicationhelper.api.resource.SampleApplicationHelperResource;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "sample-application-helper", url = "http://localhost:8080")
+@FeignClient(name = "sample-application-helper", url = "http://localhost:8081")
 public interface SampleApplicationHelperInvoker extends SampleApplicationHelperResource {
 }

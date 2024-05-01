f1799248e53621845e9f5827b5df91999f1d8e64
magistvan <magistvan@yahoo.com>
2024-05-01 13:13:36 +0300

    Generalize url for invoker

--- src/main/java/io/github/magistvan/sampleapplicationhelper/api/invoker/SampleApplicationHelperInvoker.java
+++ src/main/java/io/github/magistvan/sampleapplicationhelper/api/invoker/SampleApplicationHelperInvoker.java
@@ -3,6 +3,7 @@ package io.github.magistvan.sampleapplicationhelper.api.invoker;
 import io.github.magistvan.sampleapplicationhelper.api.resource.SampleApplicationHelperResource;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "sample-application-helper", url = "http://sample-application-helper")
+@FeignClient(name = "sample-application-helper",
+        url = "${spring.cloud.openfeign.client.config.sample-application-helper.url}")
 public interface SampleApplicationHelperInvoker extends SampleApplicationHelperResource {
 }

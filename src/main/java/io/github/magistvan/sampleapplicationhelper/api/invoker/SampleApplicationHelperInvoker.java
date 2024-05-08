007827ed64f1c6f8df344837d2c0c419d934eccd
István Mag <istvan.mag@cloudflight.io>
2024-05-08 12:37:42 +0300

    Remove url for invoker

--- src/main/java/io/github/magistvan/sampleapplicationhelper/api/invoker/SampleApplicationHelperInvoker.java
+++ src/main/java/io/github/magistvan/sampleapplicationhelper/api/invoker/SampleApplicationHelperInvoker.java
@@ -3,6 +3,6 @@ package io.github.magistvan.sampleapplicationhelper.api.invoker;
 import io.github.magistvan.sampleapplicationhelper.api.resource.SampleApplicationHelperResource;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "sample-application-helper")
+@FeignClient("sample-application-helper")
 public interface SampleApplicationHelperInvoker extends SampleApplicationHelperResource {
 }
